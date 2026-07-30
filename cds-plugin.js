8a3d1879e942ce24a8f94c7c10a87c09d38192a5
D051920 <v.kozyura@sap.com>
2026-07-30 17:34:26 +0200

    Sync wrapper for Sqlite for using ONNX embeddings function

--- cds-plugin.js
+++ cds-plugin.js
@@ -3,13 +3,25 @@ import cds from '@sap/cds';
 import enhanceModelWithRecommendations from './lib/csn-enhancements/recommendations.js';
 import registerHandlersForRecommendations from './lib/handlers/recommendations.js';
 import registerMtxHandlers from './lib/mtx/index.js';
+import addSQLiteVectorSupport from './lib/vector_handling/index.js';
 
 cds.on('compile.for.runtime', enhanceModelWithRecommendations);
 cds.on('compile.to.edmx', enhanceModelWithRecommendations);
 
 cds.on('served', async (services) => {
   for (const name in services) {
-    if (name === 'db') continue;
+    if (name === 'db') {
+      // Register vector support for SQLite
+      const db = await cds.connect.to('db');
+      if (db.kind === 'sqlite') {
+        // Access the underlying database connection
+        const dbc = db.dbc;
+        if (dbc) {
+          await addSQLiteVectorSupport(dbc);
+        }
+      }
+      continue;
+    }
     // eslint-disable-next-line no-await-in-loop
     const srv = await cds.connect.to(name);
     registerHandlersForRecommendations(srv);
